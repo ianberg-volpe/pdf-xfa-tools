7ee191f7be7b7e5228d82686e2b6b7c0e9aca5ac
Ian Berg <53795111+ianberg-volpe@users.noreply.github.com>
2023-07-11 13:09:28 -0400

    read from folder

--- xfa-extract.py
+++ xfa-extract.py
@@ -9,22 +9,25 @@ if(len(sys.argv) == 1 or re.match(r'(^-+h)',sys.argv[1]) ):
     print(f'''
 USAGE: 
 
-    python {os.path.basename(sys.argv[0])} 'PATH_TO_PDF.pdf' [... MORE_PDFS.pdf ]
+    python {os.path.basename(sys.argv[0])} 'PATH_TO_INPUT FOLDER'
   
         Output will be saved to a folder called PATH_TO_PDF (for each pdf) in the current working directory
     ''')
     quit()
 
-fileNames = sys.argv
+folderPath = sys.argv[1] # starting from 1 because the 0th arg is the file name of this script
+fileNames = os.listdir(folderPath)
 
-# starting from 1 because the 0th arg is the file name of this script
-for fileName in fileNames[1:]:
+for fileName in fileNames:
     with pikepdf.Pdf.open(fileName) as pdfData:
-        xfaDict = XfaObj(pdfData)
+        try:
+            xfaDict = XfaObj(pdfData)
+        except AttributeError:
+            continue
 
         folderName = re.sub(r'\.pdf$', '', os.path.basename(fileName))
         
-        os.makedirs(f'./{folderName}', exist_ok=True)
+        os.makedirs(f'./out/{folderName}', exist_ok=True)
         
         for key in xfaDict.keys():
             outFile = re.sub(r'[<>: ]','',key) 
