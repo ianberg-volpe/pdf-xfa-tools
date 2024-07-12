70094fec04300cd684d4e6a94351b570511234ba
Ian Berg <ian.berg@dot.gov>
2024-07-12 09:56:42 -0400

    pikepdf to pypdf

--- xfaTools.py
+++ xfaTools.py
@@ -1,8 +1,8 @@
-import pikepdf
+import pypdf
 class XfaObj(dict):
     def __init__(self, sourcePdf):
         self.source = sourcePdf
-        self.root = self.source.Root.AcroForm.XFA
+        self.root = self.source.root_object['/AcroForm']['/XFA']
         self.xfaDict = {}
         
         for i,item in enumerate(self.root):
@@ -17,7 +17,7 @@ class XfaObj(dict):
 
     def __getitem__(self,key):
         if(isinstance(self.xfaDict[key],pikepdf.Stream)):
-            return self.xfaDict[key].read_bytes().decode('utf-8')
+            return self.xfaDict[key].get_data().decode('utf-8')
         else:
             print('xfa item detected was not a stream')
             return self.xfaDict[key]
@@ -26,4 +26,4 @@ class XfaObj(dict):
     def __setitem__(self,key,value):
         if(isinstance(value,str)):
             value = bytes(value,'utf-8')
-        self.xfaDict[key].write(value)
+        self.xfaDict[key].set_data(value)
